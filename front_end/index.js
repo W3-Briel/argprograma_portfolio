f64426e684471a881eff919c552d95c0a0d799a0
briel <saucedog926@gmail.com>
2023-02-07 14:51:23 -0300

    commitFront

--- front_end/index.js
+++ front_end/index.js
@@ -1,3 +1,5 @@
+
+// MENU
 const menuIcon = document.getElementById("menu-icon-responsive");
 const menuUI = document.getElementById("menu-responsive");
 const closeMenuResponsive = document.getElementById("closeMenu-r");
@@ -16,3 +18,20 @@ function closeMenu (){
         menuUI.classList.replace("showMenu", "closeMenu");
     }
 };
+//LOGO
+
+const brand = document.getElementById("brand");
+const brandSmall = document.getElementById("brand-r");
+
+function inicio(){
+    const inicio = document.getElementById("inicio");
+    
+    if (brand.classList.contains("navbar-brand")){
+        inicio.scrollIntoView();
+    }else {
+        brandSmall.scrollIntoView();
+    }
+}
+
+brand.addEventListener("click", inicio);
+brandSmall.addEventListener("click", inicio);
